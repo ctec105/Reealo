964f1f76a1cf3e2cb825b0a8709313bc76686b25
Jossi <u20161a961@upc.edu.pe>
2019-09-14 01:13:05 -0500

    Commit 7 - Contactenos

--- app/src/main/java/com/example/reealo/fragmentos/Contacto.java
+++ app/src/main/java/com/example/reealo/fragmentos/Contacto.java
@@ -1,11 +1,17 @@
 package com.example.reealo.fragmentos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,13 +22,33 @@ public class Contacto extends Fragment {
     public Contacto() {
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_contacto,
+                container, false);
+        Button mDialButton = (Button) view.findViewById(R.id.btn_numero);
+        mDialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNo = "964240367";
+                if(!TextUtils.isEmpty(phoneNo)) {
+                    String dial = "tel:" + phoneNo;
+                    startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                }else {
+                    //Toast.makeText(Layout.fragme, "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         // le indicamos que este fragmento tiene su propio menù de opciones
-        setHasOptionsMenu(true);
+        //setHasOptionsMenu(true);
 
         // inflamos el layout para este fragmento
-        return inflater.inflate(R.layout.fragment_contacto, container, false);
+        //return inflater.inflate(R.layout.fragment_contacto  , container, false);
+        return view;
     }
 
     // TODO: Prepara el menú de opciones antes de que se muestre el menú (usado para deshabilitar o habilitar elementos)
